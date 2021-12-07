eaa625972e58a8bc2372dfcabf3d797dbf4f3698
Kim Junil <iam@junil.kim>
2021-12-07 17:01:13 +0900

    add bentom metric

--- bento/bento_service.py
+++ bento/bento_service.py
@@ -6,12 +6,18 @@ from PIL import Image
 from bentoml import api, artifacts, env, BentoService
 from bentoml.frameworks.keras import KerasModelArtifact
 from bentoml.adapters import ImageInput
+from bentoml.configuration.containers import BentoMLContainer
 
 MNIST_CLASSES = [str(x) for x in range(10)]
                
+metrics_client = BentoMLContainer.metircs_client.get()
+REQUEST_TIME = metrics_client.Summary('request_processing_time', 'Time spend processing request')
+
 @env(pip_packages=["tensorflow", "pillow", "numpy", "typing", "imageio==2.9.0"])
 @artifacts([KerasModelArtifact("model")])
 class MnistService(BentoService):
+
+    @REQUEST_TIME.time()
     @api(input=ImageInput(pilmode="L"), batch=True)
     def predict(self, imgs: List[np.ndarray]) -> List[str]:
         inputs = []
